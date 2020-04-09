8cabfa4e729a62645aa54d95af889e472a6c830a
erinduncan <58863609+erinduncan@users.noreply.github.com>
2020-04-09 18:22:10 -0400

    Fixing db.json

--- src/utility/api.ts
+++ src/utility/api.ts
@@ -4,5 +4,5 @@ import Axios from "axios";
 // of a uniform size, as these are large, high quality images
 export const imageAPI = Axios.create({
     // baseURL : "https://picsum.photos/v2/list"
-    baseURL : "https://my-json-server.typicode.com/erinduncan/CapcoProject"
+    baseURL : "https://my-json-server.typicode.com/erinduncan/CapcoProject/images"
 })
